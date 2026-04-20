874f581bbe390f6b93db9fad6c438938f2f92f6e
juliapark628 <44275477+juliapark628@users.noreply.github.com>
2026-04-20 15:07:46 -0700

    look at ppa dataset

---
+++ data_cleaning/ercot_ppa.R
@@ -0,0 +1,17 @@
+# This file calculates revenue for ESR resources.
+
+rm(list = ls())
+setwd(here::here())
+
+
+
+# SET UP
+library(tidyverse)
+library(readxl)
+
+data_path <- "data_cleaning/external/google_drive/"
+
+df <- read_excel(paste0(data_path, "2025 Utility-Scale Solar Data Update.xlsx"), sheet = "Individual_Project_Data")
+
+df_ercot <-
+  df %>% filter(Region == "ERCOT", str_detect(`Hybrid?`, "Storage"))
